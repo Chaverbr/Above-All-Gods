de1d5d0b01114d816ec6ea38cfd2db08427182c9
Caique Oliveira <caique.guimaraesdeoliveira306@gmail.com>
2023-05-16 15:04:05 -0300

    Scroll e favicon

--- versaoPlayer/scripts/index.js
+++ versaoPlayer/scripts/index.js
@@ -1,5 +1,30 @@
 import { responsivo } from "./cabecalhoResponsivo.js";
+import { smoothScrollTo } from "./smoothScroll.js";
 
 
 const iconeHamburguer = document.querySelector('[data-icone]');
 iconeHamburguer.addEventListener('click', responsivo);
+
+
+
+// SISTEMA DE ROLAGEM DA NAV 
+
+const menuBar = document.querySelectorAll('.cabeçalho-navbar a[href^="#"]');
+
+menuBar.forEach((item) => {
+    item.addEventListener('click', scrollDown);
+});
+
+
+function scrollDown(event) {
+    event.preventDefault();
+
+    const item = event.target;
+    const id = item.getAttribute('href');
+    
+    const section = document.querySelector(id).offsetTop;
+
+
+    smoothScrollTo(0, section);
+}
+
